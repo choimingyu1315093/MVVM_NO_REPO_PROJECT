bad18429132b888838b2e051c630829640d1628b
choimingyu <alsrb1315093@gmail.com>
2025-08-20 14:57:21 +0900

    프로젝트 생성

---
+++ app/src/main/java/com/example/mvvm_no_repo_project/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.mvvm_no_repo_project
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.mvvm_no_repo_project.ui.theme.MVVM_NO_REPO_PROJECTTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MVVM_NO_REPO_PROJECTTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MVVM_NO_REPO_PROJECTTheme {
+        Greeting("Android")
+    }
+}
