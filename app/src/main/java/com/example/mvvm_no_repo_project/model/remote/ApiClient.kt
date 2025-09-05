2688293fa08dd0659e967f80c1c66452e4894e41
choimingyu <alsrb1315093@gmail.com>
2025-09-05 15:39:05 +0900

    Hilt 수정

--- app/src/main/java/com/example/mvvm_no_repo_project/model/remote/ApiClient.kt
+++ app/src/main/java/com/example/mvvm_no_repo_project/model/remote/ApiClient.kt
@@ -7,24 +7,24 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
-object ApiClient {
-    private val client: OkHttpClient = OkHttpClient.Builder()
-        .connectTimeout(10, TimeUnit.SECONDS)
-        .readTimeout(10, TimeUnit.SECONDS)
-        .writeTimeout(10, TimeUnit.SECONDS)
-        .addInterceptor(HttpLoggingInterceptor().apply {
-            level = HttpLoggingInterceptor.Level.BASIC
-            //실무: DEBUG = BODY / RELEASE = BASIC (or NONE)
-        })
-        .build()
-
-    private val retrofit: Retrofit by lazy {
-        Retrofit.Builder()
-            .baseUrl(NetworkUtil.BASE_URL)
-            .client(client)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-    }
-
-    fun <T> create(service: Class<T>): T = retrofit.create(service)
-}
+//object ApiClient {
+//    private val client: OkHttpClient = OkHttpClient.Builder()
+//        .connectTimeout(10, TimeUnit.SECONDS)
+//        .readTimeout(10, TimeUnit.SECONDS)
+//        .writeTimeout(10, TimeUnit.SECONDS)
+//        .addInterceptor(HttpLoggingInterceptor().apply {
+//            level = HttpLoggingInterceptor.Level.BASIC
+//            //실무: DEBUG = BODY / RELEASE = BASIC (or NONE)
+//        })
+//        .build()
+//
+//    private val retrofit: Retrofit by lazy {
+//        Retrofit.Builder()
+//            .baseUrl(NetworkUtil.BASE_URL)
+//            .client(client)
+//            .addConverterFactory(GsonConverterFactory.create())
+//            .build()
+//    }
+//
+//    fun <T> create(service: Class<T>): T = retrofit.create(service)
+//}
